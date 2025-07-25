40848c4291f4e62c1b51500f8d2572a2daf9811f
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-07-25 00:32:26 -0600

    OPCIÓN 6 - CALCULADORA

--- actividad_07.py
+++ actividad_07.py
@@ -59,6 +59,18 @@ def max_num():
 def min_num():
     return min(list_numbers)
 
+def add_calculator(number_user_1, number_user_2):
+    return number_user_1 + number_user_2
+
+def subtrac_calculator(number_user_1, number_user_2):
+    return number_user_1 - number_user_2
+
+def multiply_calculator(number_user_1, number_user_2):
+    return number_user_1 * number_user_2
+
+def divide_calculator(number_user_1, number_user_2):
+    return number_user_1 / number_user_2
+
 while True:
     print(f"MENÚ")
     print(f"1. SUMA - PROMEDIO - POSTITIVO , NEGATIVO o CEROS, MULTIPLOS DE 3")
@@ -128,7 +140,41 @@ while True:
             list_numbers.clear()
 
         case "6":
-            print(f"CALCULADORA")
+            while True:
+                print(f"-CALCULADORA-")
+                print(f"1. Suma")
+                print(f"2. Resta")
+                print(f"3. Multiplicación")
+                print(f"4. División")
+                print(f"5. Salir")
+                option_number = input(f"Ingrese la opción que desea realizar (1-5): ")
+
+                if option_number == "5":
+                    print(f"Regresando al menú...\n")
+                    break
+
+                number_user_1 = float(input(f"Ingrese el valor del primer número: "))
+                number_user_2 = float(input(f"Ingrese el valor del segundo número: "))
+
+                match option_number:
+                    case "1":
+                        print(f"La Suma es de: {add_calculator(number_user_1, number_user_2)}\n")
+
+                    case "2":
+                        print(f"La resta es de: {subtrac_calculator(number_user_1, number_user_2)}\n")
+
+                    case "3":
+                        print(f"La multiplicación de: {multiply_calculator(number_user_1, number_user_2)}\n")
+
+                    case "4":
+                        if number_user_2 == 0:
+                            print(f"No se puede divir entre 0\n")
+
+                        else:
+                            print(f"La división es de: {divide_calculator(number_user_1, number_user_2)}\n")
+
+                    case _:
+                        print(f"valor inválido")
 
         case "7":
             print(f"Ha salido del programa, nos vemos...")
